75c88ecbb59d02c2fd8f9201b119b36417339d0e
Leissan Khan <leissan.khan@gmail.com>
2023-04-02 11:33:30 -0400

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.find_by(last_name: params[:last_name])
     render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    if student
+      render json: student
+    else
+      render json: {error: "Student not found"}
+    end
   end
 
 end
